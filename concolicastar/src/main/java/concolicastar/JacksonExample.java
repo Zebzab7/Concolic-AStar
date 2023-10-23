44aa26ba9d5bb23a1267243ded5d60aada0b0ecd
Zebzab7 <56087724+Zebzab7@users.noreply.github.com>
2023-10-23 16:41:21 +0200

    New concolicastar project

---
+++ concolicastar/src/main/java/concolicastar/JacksonExample.java
@@ -0,0 +1,54 @@
+package concolicastar;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JacksonExample {
+
+    public static void main(String[] args) {
+        try {
+            // Create a simple object for demonstration
+            Person person = new Person("John", 30);
+
+            // Convert Java object to JSON string
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonString = objectMapper.writeValueAsString(person);
+            System.out.println("JSON Representation: " + jsonString);
+
+            // Convert JSON string back to Java object
+            Person parsedPerson = objectMapper.readValue(jsonString, Person.class);
+            System.out.println("Parsed Person's Name: " + parsedPerson.getName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Person {
+        private String name;
+        private int age;
+
+        // Necessary for Jackson
+        public Person() {
+        }
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+}
