30a7a1a3eb60d41d45736659d560fc9ba0ba3947
shreyassgs00 <shreklife00@gmail.com>
2023-10-23 16:52:36 +0200

    Removed redundant folder

---
+++ concolicastar/src/main/java/dtu/project/Car.java
@@ -0,0 +1,13 @@
+package dtu.project;
+
+public class Car {
+
+    public String color;
+    public String type;
+
+    // standard getters setters
+        public Car(String color, String type) {
+            this.color = color;
+            this.type = type;
+        }
+}
