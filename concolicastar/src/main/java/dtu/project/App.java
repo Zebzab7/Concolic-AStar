3acb11b866d9524a177a9fd32a73a743a1214b14
Collinnn <51782585+Collinnn@users.noreply.github.com>
2023-10-23 14:18:03 +0200

    Working finding JSON files

--- concolicastar/src/main/java/dtu/project/App.java
+++ concolicastar/src/main/java/dtu/project/App.java
@@ -1,5 +1,9 @@
 package dtu.project;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        //findFiles(path);
+        ArrayList<File> files;
+        files = Folders.findFiles("course-02242-examples-main");
+        for(File file : files){
+            System.out.println(file.getPath());
+        }
     }
 }
