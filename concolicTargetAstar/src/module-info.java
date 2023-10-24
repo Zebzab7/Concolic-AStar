e5afe14bd5e9c534f41d08bcd4e0f22c43cd8987
s220056 <s220056@student.dtu.dk>
2023-10-24 17:09:58 +0200

    map simple json successfully progress in JsonObject

--- concolicTargetAstar/src/module-info.java
+++ concolicTargetAstar/src/module-info.java
@@ -6,4 +6,9 @@
  *
  */
 module concolicTargetAstar {
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
+
+    exports dtu.project;
 }
