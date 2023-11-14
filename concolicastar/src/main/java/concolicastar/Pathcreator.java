c577c85f16a1e648104967459e853d22ec9789fd
Collinnn <s183713@student.dtu.dk>
2023-11-14 19:35:14 +0100

    Very Rough framework

---
+++ concolicastar/src/main/java/concolicastar/Pathcreator.java
@@ -0,0 +1,58 @@
+package concolicastar;
+
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class Pathcreator {
+
+    ArrayList<Bytecode> bytecodes = new ArrayList<Bytecode>();
+    AbsoluteMethod startam;
+    
+    public Pathcreator(ArrayList<JsonFile> files, AbsoluteMethod am) {
+        this.startam =am;
+        for(JsonFile file : files) {
+            JSONArray methods = file.getMethods();
+            for (Object obj : methods) {
+                JSONObject method = (JSONObject) obj;
+                String methodName = (String) method.get("name");
+                JSONObject code = (JSONObject) method.get("code");
+                Bytecode bc = new Bytecode(file.getFileName(), methodName , (JSONArray) code.get("bytecode"));
+                BootstrapMethods bm = new BootstrapMethods(file.getFileName(), methodName, (JSONArray) method.get("bootstrapMethods"));
+                bytecodes.add(bc);
+
+            }
+    }
+
+    public void findPath(int EndIndex,AbsoluteMethod am){
+        Bytecode bc = Interpreter.findMethod(am);
+        int pc = 0;
+        JSONObject bytecode;
+        int size = bc.getBytecode().size();
+        String opr = "";
+
+        //Wont handle multiple methods calling this method for now
+        while (pc < size) {
+            bytecode = (JSONObject) bc.getBytecode().get(pc);
+            opr = (String)bytecode.get("opr");
+            if(am.getMethodName().equals("main") && pc == size ){
+                return;
+            }
+            if(pc == EndIndex && am.getMethodName().equals(startam.getMethodName())){
+                System.out.println("Found Start");
+                return;
+            }
+            
+            if(opr.equals("if") || opr.equals("ifz")){
+                //Split into 2 different worklists
+            }
+
+            pc++;
+        }
+        
+
+
+
+    }
+}
--- concolicastar/src/main/java/concolicastar/app.java
+++ concolicastar/src/main/java/concolicastar/app.java
@@ -19,6 +19,7 @@ public class App
         System.out.println( "Hello World!" );
         ArrayList<JsonFile> files;
         files = Folders.findFiles("course-02242-examples-main/decompiled/dtu/compute/exec/",".json");
+
         Interpreter interpreter = new Interpreter(files);
         // interpreter.interpret("method");
         Tests.testList(interpreter);
