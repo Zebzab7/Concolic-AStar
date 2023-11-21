a9e6cef337c3eee3ac68fdc64b2257dd9f849171
Shreyas Srikanth <shreklife00@gmail.com>
2023-11-21 23:08:06 +0530

    Added more cases for banking system

--- banking_system/src/main/java/banking_system/ResourceManager.java
+++ banking_system/src/main/java/banking_system/ResourceManager.java
@@ -1,45 +1,7 @@
 package banking_system;
 public class ResourceManager {
-    public void manageResource(User user, Permission permission) {
-        if ("admin".equals(user.getRole())) {
-            adminOperation(permission);
-        } else if ("manager".equals(user.getRole())) {
-            managerOperation(permission);
-        } else {
-            regularOperation(permission);
-        }
-    }
-
-    private void adminOperation(Permission permission) {
-        if (permission.getAccessLevel() == AccessLevel.HIGH) {
-            System.out.println("Admin performed a high-level operation.");
-        } else if (permission.getAccessLevel() == AccessLevel.MEDIUM) {
-            System.out.println("Admin performed a medium-level operation.");
-        } else {
-            System.out.println("Admin performed a low-level operation.");
-        }
-        // ... other admin-specific operations
-    }
-
-    private void managerOperation(Permission permission) {
-        if (permission.getAccessLevel() == AccessLevel.HIGH) {
-            System.out.println("Manager performed a high-level operation.");
-        } else if (permission.getAccessLevel() == AccessLevel.MEDIUM) {
-            System.out.println("Manager performed a medium-level operation.");
-        } else {
-            System.out.println("Manager performed a low-level operation.");
-        }
-        // ... other manager-specific operations
-    }
-
-    private void regularOperation(Permission permission) {
-        if (permission.getAccessLevel() == AccessLevel.HIGH) {
-            System.out.println("User performed a high-level operation.");
-        } else if (permission.getAccessLevel() == AccessLevel.MEDIUM) {
-            System.out.println("User performed a medium-level operation.");
-        } else {
-            System.out.println("User performed a low-level operation.");
-        }
-        // ... other regular user operations
+    public void manageResource(User user, Permission permission, UserOperation operation) {
+        UserOperationHandler userOperationHandler = new UserOperationHandler(user, permission, operation);
+        userOperationHandler.handle();
     }
 }
--- banking_system/src/main/java/banking_system/User.java
+++ banking_system/src/main/java/banking_system/User.java
@@ -11,4 +11,20 @@ public class User {
     public String getRole() {
         return role;
     }
+
+    public void checkPermission(User user, Permission permission) {
+        if ("user".equals(user.getRole()) && !"LOW".equals(permission.getAccessLevel().toString())) {
+            System.out.println("User must not have high access level.");
+            System.exit(0);
+            return;
+        } else if ("manager".equals(user.getRole()) && "LOW".equals(permission.getAccessLevel().toString())) {
+            System.out.println("Manager must not have low access level.");
+            System.exit(0);
+            return;
+        } else if ("admin".equals(user.getRole()) && "LOW".equals(permission.getAccessLevel().toString())) {
+            System.out.println("Admin must not have low access level.");
+            System.exit(0);
+            return;
+        }
+    }
 }
